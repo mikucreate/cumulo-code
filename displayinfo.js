20c086cf95bb9d47648fe6ec3d8d7a8155daf1cc
mikucreate <lishgins@gmail.com>
2025-02-19 12:54:38 -0800

    Add files via upload
    This code uses display.Info as a basic console.log in cumulo coding.

---
+++ displayinfo.js
@@ -0,0 +1,17 @@
+let display = {
+  logs: [], // Clear any previous logs
+};
+
+// Define display.info as a function that logs messages
+display.info = function(...args) {
+  const message = args.join(" "); // Join all arguments into a single message
+  this.logs.push(message); // Add the message to the logs array
+  console.log(message); // Log the message immediately to the console
+};
+
+display.info('hi');
+
+
+
+
+
